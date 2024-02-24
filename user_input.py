040e73928b09665b580dc55797df17e45a53535f
WalterKiplangat <rutohwalters@gmail.com>
2024-02-24 16:35:35 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask the user for their name
+name: str = input("What is your name? ")
+
+# Ask the user for their age
+age = input("How old are you? ")
+
+# Ask the user for their location
+location = input("Where do you live? ")
+
+# Print out a personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
